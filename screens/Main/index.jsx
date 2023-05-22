e69024c49cb167810b679fdccb86b463868a61f9
Lab 3 <matheusdanielgomess@gmail.com>
2023-05-22 19:49:23 -0400

    Initial commit

---
+++ screens/Main/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { BarChart, Grid } from 'react-native-svg-charts';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+export default Main;
